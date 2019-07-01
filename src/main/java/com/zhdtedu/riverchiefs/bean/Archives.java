b58035546ff3ae64fcd228aa0c5a72c9de2dfc15
yuanhc <yuanhc666@163.com>
2019-07-01 14:13:20 +0800

    init

--- src/main/java/com/zhdtedu/riverchiefs/bean/Archives.java
+++ src/main/java/com/zhdtedu/riverchiefs/bean/Archives.java
@@ -9,14 +9,14 @@ import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
 @Table(name = "ri_archives")
 public class Archives {
 
-    @Column(name = "id",type = MySqlTypeConstant.INT,length = 11,isKey = true,isAutoIncrement = true)
+    @Column(name = "id", type = MySqlTypeConstant.INT, length = 11, isKey = true, isAutoIncrement = true)
     private int id;
 
-    @Column(name = "ajbm",type = MySqlTypeConstant.VARCHAR,length = 100)
+    @Column(name = "ajbm", type = MySqlTypeConstant.VARCHAR, length = 100)
     private String ajbm;
 
 
-    @Column(name = "sfwz",type = MySqlTypeConstant.VARCHAR,length = 200)
+    @Column(name = "sfwz", type = MySqlTypeConstant.VARCHAR, length = 200)
     private String sfwz;
     private String hrhm;
     private String jbrxm;
