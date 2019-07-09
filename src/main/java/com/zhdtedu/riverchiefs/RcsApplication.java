ea31c3d97d0e84334ca96e70ae74e1bdd4a8a6d6
yuanhc <yuanhc666>
2019-07-09 16:16:12 +0800

    up

--- src/main/java/com/zhdtedu/riverchiefs/RcsApplication.java
+++ src/main/java/com/zhdtedu/riverchiefs/RcsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.zhdtedu.riverchiefs.dao.*")
+@MapperScan({"com.zhdtedu.riverchiefs.dao.*","com.zhdtedu.core.dao"})
 public class RcsApplication {
     public static void main(String[] args) {
         SpringApplication.run(RcsApplication.class, args);
