17a5f0b74a7ad3fa63d465318ec5e768d0998427
xuy <xuy@zhdtedu.com>
2019-07-12 14:16:41 +0800

    统一包目录2

---
+++ src/main/java/com/zhdtedu/riverchiefs/config/ApiVersion.java
@@ -0,0 +1,20 @@
+package com.zhdtedu.riverchiefs.config;
+
+
+import org.springframework.web.bind.annotation.Mapping;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Mapping
+public @interface ApiVersion {
+    /**
+     * 标识版本号
+     *
+     * @return
+     */
+    double value();
+
+}
