617cf45c5c4d364c9414463dedf395fd9f52e86f
wujl <123456789@qq.com>
2019-07-16 11:26:57 +0800

    基础controller，添加参数获取方法

---
+++ src/main/java/com/zhdtedu/riverchiefs/controller/BaseController.java
@@ -0,0 +1,62 @@
+package com.zhdtedu.riverchiefs.controller;
+
+import com.zhdtedu.util.SearchCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Iterator;
+import java.util.Map;
+
+public class BaseController {
+
+    @Autowired
+    public HttpServletRequest request;
+
+   /* protected EcUser getUserInfo(){
+        Visit v = (Visit) request.getSession().getAttribute("VISIT");
+        return (EcUser) v.getLoginUser();
+    }
+
+    protected EcOrgInfo getEcOrgInfo(){
+        Visit v = (Visit) request.getSession().getAttribute("VISIT");
+        return (EcOrgInfo) v.getLoginOrg();
+    }
+*/
+
+    protected SearchCondition getSearchCondition(){
+        SearchCondition sc = new SearchCondition(this.request);
+//        sc.setUser(this.getUserInfo());
+//        sc.setOrgInfo(this.getEcOrgInfo());
+        StringBuffer sb = new StringBuffer();
+        for (Iterator<?> i = this.request.getParameterMap().keySet().iterator(); i
+                .hasNext();) {
+            String key = (String) i.next();
+            if(key.startsWith("sort")){
+                sb.append(request.getParameter(key)+" ");
+            }
+            if(key.startsWith("order")){
+                sb.append(request.getParameter(key));
+            }
+            if(key.startsWith("page_number")){
+                sc.setPageNo(Integer.parseInt(request.getParameter(key)));
+            }
+            if(key.startsWith("page_length")){
+                sc.setPageSize(Integer.parseInt(request.getParameter(key)));
+            }
+        }
+        sc.setOrderBy(sb.toString());
+        return sc;
+    }
+
+
+
+//    public ReqParam getReqParam(Map<String, Object> param){
+//        return new ReqParam(param);
+//    }
+//
+//    public ReturnMsg getReturnMsg() {
+//        return new ReturnMsg();
+//    }
+
+
+}
