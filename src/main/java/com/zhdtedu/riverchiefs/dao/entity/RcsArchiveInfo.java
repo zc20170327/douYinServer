299d96ac155bcca2235aed9b9d6c5fbdd8a091b3
wujl <123456789@qq.com>
2019-07-17 14:03:11 +0800

    案卷登记entity统一字段格式

--- src/main/java/com/zhdtedu/riverchiefs/dao/entity/RcsArchiveInfo.java
+++ src/main/java/com/zhdtedu/riverchiefs/dao/entity/RcsArchiveInfo.java
@@ -4,69 +4,75 @@ import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
 import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
 import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Data
 @Table(name="rcs_archive_info")
 public class RcsArchiveInfo {
 
     @Column(name="ID_",length = 11,isKey = true,type=MySqlTypeConstant.INT,isAutoIncrement =true)
+    @Getter @Setter
     private Integer id;
 
-    @Column(name="ARCH_NUM",type =MySqlTypeConstant.VARCHAR,length = 32)
+    @Column(name="ARCH_NUM_",type =MySqlTypeConstant.VARCHAR,length = 32)
     private String archNum;
 
-    @Column(name="QUESTION_FROM",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="QUESTION_FROM_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String questionFrom;
 
-    @Column(name="QUESTION_TYPE",type =MySqlTypeConstant.VARCHAR,length =20)
+    @Column(name="QUESTION_TYPE_",type =MySqlTypeConstant.VARCHAR,length =20)
     private String questionType;
 
-    @Column(name="QUESTION_ADDR",type =MySqlTypeConstant.VARCHAR,length = 128)
+    @Column(name="QUESTION_ADDR_",type =MySqlTypeConstant.VARCHAR,length = 128)
     private String questionAddr;
 
-    @Column(name="MAP_LEVEL",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="MAP_LEVEL_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String mapLevel;
 
-    @Column(name="MAP_ADDR",type =MySqlTypeConstant.VARCHAR,length = 30)
+    @Column(name="MAP_ADDR_",type =MySqlTypeConstant.VARCHAR,length = 30)
     private String mapAddr;
 
-    @Column(name="SUOS_LIUY",type =MySqlTypeConstant.VARCHAR,length = 64)
+    @Column(name="SUOS_LIUY_",type =MySqlTypeConstant.VARCHAR,length = 64)
     private String suosLiuy;
 
-    @Column(name="CALL_PHONE",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="CALL_PHONE_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String callPhone;
 
-    @Column(name="HDZ_NUM",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="HDZ_NUM_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String hdzNum;
 
-    @Column(name="HDZ_REALNAME",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="HDZ_REALNAME_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String hdzRealname;
 
-    @Column(name="HDZ_PHONE",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="HDZ_PHONE_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String hdzPhone;
 
-    @Column(name="JBR_REALNAME",type =MySqlTypeConstant.VARCHAR,length = 12)
+    @Column(name="JBR_REALNAME_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String jbrRealname;
 
-    @Column(name="DESCRIPTION",type =MySqlTypeConstant.VARCHAR)
+    @Column(name="DESCRIPTION_",type =MySqlTypeConstant.VARCHAR)
     private String description;
 
-    @Column(name="REPORT_ID",type =MySqlTypeConstant.INT,length=11)
+    @Column(name="REPORT_ID_",type =MySqlTypeConstant.INT,length=11)
     private String report_id;
 
-    @Column(name="REPORT_REALNAME",type =MySqlTypeConstant.VARCHAR,length = 128)
+    @Column(name="REPORT_REALNAME_",type =MySqlTypeConstant.VARCHAR,length = 128)
     private String reportRealname;
 
-    @Column(name="REPORT_DATE",type =MySqlTypeConstant.DATETIME,length = 7)
+    @Column(name="REPORT_DATE_",type =MySqlTypeConstant.DATETIME,length = 7)
     private String reportDate;
 
     @Column(name="STATUS_",type =MySqlTypeConstant.VARCHAR,length = 12)
     private String status;
 
-    @Column(name="EXT1",type =MySqlTypeConstant.VARCHAR,length =50)
+    @Column(name="EXT1_",type =MySqlTypeConstant.VARCHAR,length =50)
     private String ext1;
 
-    @Override
+
+
+   /* @Override
     public String toString() {
         return "RcsArchiveInfo{" +
                 "id=" + id +
@@ -89,5 +95,5 @@ public class RcsArchiveInfo {
                 ", status='" + status + '\'' +
                 ", ext1='" + ext1 + '\'' +
                 '}';
-    }
+    }*/
 }
