167e50cf2b66dfaa06a95380d9d52efa6e0eddaf
xuy <xuy@zhdtedu.com>
2019-07-22 13:23:03 +0800

    应用程序修改跨域问题

--- src/main/java/com/zhdtedu/RcsApplication.java
+++ src/main/java/com/zhdtedu/RcsApplication.java
@@ -3,6 +3,10 @@ package com.zhdtedu;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 @MapperScan({"com.zhdtedu.riverchiefs.dao.*","com.zhdtedu.system.dao"})
@@ -12,4 +16,25 @@ public class RcsApplication {
     public static void main(String[] args) {
         SpringApplication.run(RcsApplication.class, args);
     }
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     *
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+
 }
